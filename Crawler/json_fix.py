e52277aa334ab083848745fbd636eec2ef2e389b
ferokuk <gneushevyar@mail.ru>
2022-10-16 14:54:13 +0300

    updated readme

--- Crawler/json_fix.py
+++ Crawler/json_fix.py
@@ -1,10 +1,22 @@
 import json
+
+
 amount = 0
-with open("new_m2.json","w",encoding="utf-8") as f:
-  with open ("m2.json") as raw_dict:
+with open("machinist_result.json","w",encoding="utf-8") as f:
+  with open ("m.json") as raw_dict:
     d = json.load(raw_dict)
     for v in d:
       amount += 1
     for v in json.JSONEncoder(ensure_ascii=False,indent=4).iterencode(d):
       f.write(v)
-print(f"[+] Total vacancies: {amount}")
+print(f"[+] Total machinist vacancies: {amount}")
+
+amount = 0
+with open("cooks_result.json","w",encoding="utf-8") as f:
+  with open ("c.json") as raw_dict:
+    d = json.load(raw_dict)
+    for v in d:
+      amount += 1
+    for v in json.JSONEncoder(ensure_ascii=False,indent=4).iterencode(d):
+      f.write(v)
+print(f"[+] Total cooks vacancies: {amount}")
--- README.md
+++ README.md
@@ -8,6 +8,8 @@ If you want to run script:
 
 2) run "pip install -r requirements.txt"
 
-3) run "scrapy crawl cook_resume_spider -O your_file_name.csv" to get all visible cook resumes
+3) run "scrapy crawl cooks_spider -O c.json" to get all visible cook resumes
 
-4) run "scrapy crawl machinist_resume_spider -O your_file_name.csv" to get all visible machinist resumes
+4) run "scrapy crawl machinists_spider -O m.json" to get all visible machinist resumes
+
+5) run "python json_fix.py" to get 2 result files with all vacancies
